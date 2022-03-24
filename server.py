13e69d8fcb8e74e366dd4ebc8a51984b2be52f80
Darah Aldridge <darah.aldridge@gmail.com>
2022-03-24 15:48:38 -0500

    finished assignment

--- server.py
+++ server.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)  
-
+app.secret_key = "Let's Buy Fruit!"
 @app.route('/')         
 def index():
     return render_template("index.html")
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
+    total = int(request.form['apple']) + int(request.form['raspberry']) + int(request.form['strawberry'])
+    session['first_name'] = request.form['first_name']
+    session['last_name'] = request.form['last_name']
+    session['student_id'] = request.form['student_id']
+    session['student_id'] = request.form['student_id']
+    session['apple'] = request.form['apple']
+    session['strawberry'] = request.form['strawberry']
+    session['raspberry'] = request.form['raspberry']
+    session['total'] = total
+    print("Charging", session['first_name'], session['last_name'], "for", session['total'], "fruits")
+    return redirect("/checking-out")
+
+@app.route('/checking-out')
+def checkingOut():
     return render_template("checkout.html")
 
 @app.route('/fruits')         
